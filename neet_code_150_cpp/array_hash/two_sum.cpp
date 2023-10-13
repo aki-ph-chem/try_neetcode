121c127f3d619f18178a8d9077d190065774a1e8
aki <aki@mymail.com>
2023-10-14 01:52:26 +0900

    Two Sum: Ok

--- neet_code_150_cpp/array_hash/two_sum.cpp
+++ neet_code_150_cpp/array_hash/two_sum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <vector>
 #include <unordered_map>
 
@@ -19,18 +20,41 @@ class Solution {
             return result;
         }
 
-        // O(N^2)より良いオーダーで書きたい
         std::vector<int> two_sum_2(const std::vector<int>& nums, int target) {
-            std::unordered_map<int, int> map;
+            std::unordered_map<int, std::size_t> map;
             std::vector<int> result;
             for(std::size_t i = 0; i < nums.size(); ++i) {
                 auto diff = target - nums[i];
+
                 if(map.find(diff) != map.end()) {
+                    auto j = map[diff];
+                    result.push_back(i);
+                    result.push_back(j);
+                } else {
+                    map.insert(std::make_pair(nums[i], i));
                 }
             }
 
             return result;
         }
+};
+
+// 模範解答
+class SolutionAns {
+    public:
+        std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        int n = nums.size();
+        std::unordered_map<int, int> mp; // val -> index
+
+        for (int i = 0; i < n; i++) {
+            int compliment = target - nums[i];
+            if (mp.find(compliment) != mp.end()) {
+                return {mp[compliment], i};
+            }
+            mp.insert({nums[i], i});
+        }
+        return {};
+    }
 };
 
 int main(void) {
@@ -44,6 +68,7 @@ int main(void) {
     auto target_3 = 6;
 
     Solution s_1;
+    std::cout << "two_sum()"<< std::endl;
     for(const auto &v: s_1.two_sum(nums_1, target_1)) {
         std::cout << v <<" ";
     }
@@ -53,6 +78,35 @@ int main(void) {
     }
     std::cout << std::endl;
     for(const auto &v: s_1.two_sum(nums_3, target_3)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "two_sum_2()"<< std::endl;
+    for(const auto &v: s_1.two_sum_2(nums_1, target_1)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+    for(const auto &v: s_1.two_sum_2(nums_2, target_2)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+    for(const auto &v: s_1.two_sum_2(nums_3, target_3)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+
+    SolutionAns s_2;
+    std::cout << "twoSum()"<< std::endl;
+    for(const auto &v: s_2.twoSum(nums_1, target_1)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+    for(const auto &v: s_2.twoSum(nums_2, target_2)) {
+        std::cout << v <<" ";
+    }
+    std::cout << std::endl;
+    for(const auto &v: s_2.twoSum(nums_3, target_3)) {
         std::cout << v <<" ";
     }
     std::cout << std::endl;
