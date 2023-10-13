367b8130b4e72836353e19a2dcc0525abafad18c
aki <aki@mymail.com>
2023-10-13 16:28:39 +0900

    HashMapの例

--- neet_code_150/src/appendix/hash_map.rs
+++ neet_code_150/src/appendix/hash_map.rs
@@ -12,4 +12,16 @@ fn main() {
     for v in map.iter() {
         println!("v : {:?}", v);
     }
+
+    let s_2 = "nagaram".to_string();
+
+    // map.entry(x).or_dfault() += 1
+    // xが含まれていたらなにもせずにvalueを1増やす
+    // 一方、含まれていないならば、(x,0)を挿入してvalueを1増やす(1にする)
+    let mut map_2:HashMap<char, i64> = HashMap::new();
+    for (a,b) in s_1.chars().zip(s_2.chars()) {
+        *map_2.entry(a).or_default() += 1;
+        *map_2.entry(b).or_default() -= 1;
+        println!("map_2: {:?}", map_2);
+    }
 }
