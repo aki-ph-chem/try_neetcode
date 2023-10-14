e307b298c42f873b43349f0e55933d10cc60e49b
aki <aki@mymail.com>
2023-10-14 23:13:03 +0900

    Top K Frequent Element

---
+++ neet_code_150_cpp/array_hash/top_k_freq.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+//#define DEBUG_ANS
+
+class Solution {
+    public:
+        std::vector<int> top_k_freq(const std::vector<int>& nums, int k) {
+            std::unordered_map<int,int> map;
+
+            for(const auto &n : nums) {
+                if(map.find(n) != map.end()) {
+                    map.insert(std::make_pair(n, 0));
+                }
+                ++map[n];
+            }
+
+            /*
+            for(const auto &v: map) {
+                std::cout << "(" << v.first << "," << v.second << ")" << std::endl;
+            }
+            */
+
+            return std::vector{1,2};
+        }
+};
+
+
+class SolutionAns {
+    public:
+        std::vector<int> topKFrequent(std::vector<int>& nums, int k) {
+            int n = nums.size();
+
+            std::unordered_map<int, int> m;
+            for (int i = 0; i < n; i++) {
+                m[nums[i]]++;
+            }
+#ifdef DEBUG_ANS
+            std::cerr << "m" << std::endl;
+            for(const auto &v: m) {
+                std::cout << "(" << v.first << "," << v.second << ")" << std::endl;
+            }
+#endif
+            std::vector<std::vector<int>> buckets(n + 1);
+            for (auto it = m.begin(); it != m.end(); it++) {
+                buckets[it->second].push_back(it->first);
+            }
+#ifdef DEBUG_ANS
+            std::cerr << "bukets" << std::endl;
+            for(const auto &v: buckets) {
+                for(const auto &w: v) {
+                    std::cerr << w << std::endl;
+                }
+            }
+#endif
+            std::vector<int> result;
+
+            for (int i = n; i >= 0; i--) {
+                if (static_cast<int>(result.size()) >= k) {
+                    break;
+                }
+                if (!buckets[i].empty()) {
+                    // bukets[i]の要素をresultに後ろから挿入
+                    result.insert(result.end(), buckets[i].begin(), buckets[i].end());
+                }
+            }
+
+            return result;
+        }
+};
+
+int main(void) {
+    auto case_1_array = std::vector{1,1,1,2,2,3};
+    auto case_1_k = 2;
+
+    SolutionAns s_2;
+    auto res_case_1_ans = s_2.topKFrequent(case_1_array, case_1_k);
+    for(const auto &v: res_case_1_ans) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+}
