21b652efe88369b435d7b181b679c9ecf38aa04e
aki <aki@mymail.com>
2023-10-16 04:09:37 +0900

    Longest Substring Without Repeating Characters: まだ途中

---
+++ neet_code_150/src/sliding_window/longest_substring_no_repeat.rs
@@ -0,0 +1,13 @@
+struct Solution {}
+impl Solution {
+    pub fn length_of_longest_substring(s: String) -> i32 {
+        3
+    }
+}
+
+struct SolutionAns {}
+fn main() {
+    let case_1 = "abcabcbb".to_string();
+    let case_2 = "bbbbb".to_string();
+    let case_3 = "pwwkew".to_string();
+}
