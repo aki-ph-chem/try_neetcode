a52e8c8734e82e5691178e07aee2ca54d2944a5c
aki <aki@mymail.com>
2023-10-24 14:34:24 +0900

    Longest Substring Without Rpeating Characters: Rust

--- neet_code_150/src/sliding_window/longest_substring_no_repeat.rs
+++ neet_code_150/src/sliding_window/longest_substring_no_repeat.rs
@@ -1,7 +1,59 @@
+use std::collections::HashSet;
+
 struct Solution {}
 impl Solution {
     pub fn length_of_longest_substring(s: String) -> i32 {
-        3
+        if s.len() == 0 {
+            return 0;
+        }
+        let mut set = HashSet::new();
+        let mut len_list = vec![];
+        let mut len_tmp = 0;
+        for c in s.chars() {
+            if !set.contains(&c) {
+                set.insert(c);
+                len_tmp += 1;
+            } else {
+                len_list.push(len_tmp);
+                len_tmp = 1;
+                set.clear();
+                set.insert(c);
+            }
+        }
+        len_list.push(len_tmp);
+
+        println!("len_list: {:?}", len_list);
+        *len_list.iter().max().unwrap()
+    }
+
+    // AC : O(N^2)
+    pub fn length_of_longest_substring_sq(s: String) -> i32 {
+        if s.len() == 0 {
+            return 0;
+        }
+        let s_vec: Vec<char> = s.chars().collect();
+
+        let mut set = HashSet::new();
+        let mut len_list = vec![];
+        let mut len_tmp = 1;
+
+        for i in 0..s_vec.len() {
+            set.insert(s_vec[i]);
+            for j in (i + 1)..s_vec.len() {
+                if !set.contains(&s_vec[j]) {
+                    set.insert(s_vec[j]);
+                    len_tmp += 1;
+                } else {
+                    break;
+                }
+            }
+            len_list.push(len_tmp);
+            set.clear();
+            len_tmp = 1;
+        }
+
+        println!("len_list: {:?}", len_list);
+        *len_list.iter().max().unwrap()
     }
 }
 
@@ -10,4 +62,68 @@ fn main() {
     let case_1 = "abcabcbb".to_string();
     let case_2 = "bbbbb".to_string();
     let case_3 = "pwwkew".to_string();
+    let case_4 = "xyz".to_string();
+    let case_5 = "au".to_string();
+    let case_6 = "a".to_string();
+    let case_7 = "dvdf".to_string();
+
+    println!("lenght_of_longest_substring");
+    println!(
+        "case_1: {}",
+        Solution::length_of_longest_substring(case_1.clone())
+    );
+    println!(
+        "case_2: {}",
+        Solution::length_of_longest_substring(case_2.clone())
+    );
+    println!(
+        "case_3: {}",
+        Solution::length_of_longest_substring(case_3.clone())
+    );
+    println!(
+        "case_4: {}",
+        Solution::length_of_longest_substring(case_4.clone())
+    );
+    println!(
+        "case_5: {}",
+        Solution::length_of_longest_substring(case_5.clone())
+    );
+    println!(
+        "case_6: {}",
+        Solution::length_of_longest_substring(case_6.clone())
+    );
+    println!(
+        "case_7: {}",
+        Solution::length_of_longest_substring(case_7.clone())
+    );
+
+    println!("lenght_of_longest_substring_sq");
+    println!(
+        "case_1: {}",
+        Solution::length_of_longest_substring_sq(case_1.clone())
+    );
+    println!(
+        "case_2: {}",
+        Solution::length_of_longest_substring_sq(case_2.clone())
+    );
+    println!(
+        "case_3: {}",
+        Solution::length_of_longest_substring_sq(case_3.clone())
+    );
+    println!(
+        "case_4: {}",
+        Solution::length_of_longest_substring_sq(case_4.clone())
+    );
+    println!(
+        "case_5: {}",
+        Solution::length_of_longest_substring_sq(case_5.clone())
+    );
+    println!(
+        "case_6: {}",
+        Solution::length_of_longest_substring_sq(case_6.clone())
+    );
+    println!(
+        "case_7: {}",
+        Solution::length_of_longest_substring_sq(case_7.clone())
+    );
 }
