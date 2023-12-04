525a0368fc3c42477180b3c254667e2750ac30a9
aki <aki@mymail.com>
2023-12-04 09:18:01 +0900

    Jump Game: まだ途中

---
+++ neet_code_150/src/greedy/jump_game_2.rs
@@ -0,0 +1,17 @@
+struct Solution {}
+impl Solution {
+    pub fn jump(nums: Vec<i32>) -> i32 {
+        let mut result = 0;
+        for i in 0..nums.len() {
+        }
+
+        result
+    }
+}
+
+fn main() {
+    let case_1 = vec![2, 3, 1, 1, 4];
+    let case_2 = vec![3, 2, 1, 0, 4];
+    let case_3 = vec![2, 0];
+    let case_4 = vec![2,5,0,0];
+}
