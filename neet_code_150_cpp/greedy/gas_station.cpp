3fd2cfd41d2abae44bc60be5e7cf59990f0ed4d6
aki <aki@mymail.com>
2023-12-06 19:09:10 +0900

    Gas Station: cpp まだ途中

---
+++ neet_code_150_cpp/greedy/gas_station.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+    public:
+        int canCompleteCircuit(std::vector<int>& gas, std::vector<int> cost) {
+
+            return 1;
+        }
+};
+
+int main(void) {
+}
