248e7114be2fee2844ecaa5262230c648fa416e9
aki <aki@mymail.com>
2023-12-14 19:34:19 +0900

    House Robber

--- neet_code_150/src/one_d_dp/climbing_stair_cost.rs
+++ neet_code_150/src/one_d_dp/climbing_stair_cost.rs
@@ -28,6 +28,20 @@ impl Solution {
     }
 }
 
+// 模範解答
+struct SolutionAns {}
+impl SolutionAns {
+    pub fn min_cost_climbing_stairs(mut cost: Vec<i32>) -> i32 {
+        for i in 2..cost.len(){
+            cost[i] += cost[i- 1].min(cost[i-2]);
+        }
+
+        let len = cost.len();
+
+        cost[len - 1].min(cost[len - 2])
+    }
+}
+
 fn main() {
     let case_1 = vec![10, 15, 20];
     let case_2 = vec![1, 100, 1, 1, 1, 100, 1, 1, 100, 1];
