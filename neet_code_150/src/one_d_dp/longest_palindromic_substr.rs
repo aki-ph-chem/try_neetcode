5d5d4b4a9d882674a9f179881481e5e9c1e1c3c7
aki <aki@mymail.com>
2023-12-16 01:22:04 +0900

    Longest Palindromic Substring

--- neet_code_150/src/one_d_dp/longest_palindromic_substr.rs
+++ neet_code_150/src/one_d_dp/longest_palindromic_substr.rs
@@ -3,9 +3,25 @@
 // Hint 3: 部分文字列の列挙でO(N^2), 各区間の回文判定にO(N)
 // 最後の回文判定をキャッシュ化できる
 
-struct Solution{}
+struct Solution {}
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
+        let n = s.len();
+        let str: Vec<char> = s.chars().collect();
+        let mut len_list: Vec<((usize, usize), i32)> = vec![];
+
+        // i < j
+        for i in 0..n {
+            for j in (i + 1)..n {
+                // i <= k <=j
+                let mut len_sub_str = 0;
+
+                // 区間[i,j]が回文か判定
+                println!("{:?}",((i, j), len_sub_str));
+                len_list.push(((i, j), len_sub_str));
+            }
+        }
+
         "hoge".to_string()
     }
 
@@ -17,14 +33,15 @@ impl Solution {
 
         // i < j
         for i in 0..n {
-            for j in i + 1 ..n {
+            for j in i + 1..n {
                 // i <= k <=j
                 let mut len_sub_str = 0;
                 // 区間[i,j]が回文か判定
-                for k in i..(j + 1) {
-                    if str[k] == str[j - k] {
+                for k in 0..(j - i + 1){
+                    if str[i + k] == str[j - k] {
                         len_sub_str += 1;
                     } else {
+                        len_sub_str = -1;
                         break;
                     }
                 }
@@ -34,14 +51,101 @@ impl Solution {
 
         *len_list.iter().max().unwrap()
     }
+
+    // 最大の回文を返す
+    pub fn palindrome_max_len(s: String) -> ((usize, usize, i32), String) {
+        let n = s.len();
+        let str: Vec<char> = s.chars().collect();
+        let mut len_list: Vec<((usize, usize), i32)> = vec![];
+
+        // i < j
+        for i in 0..n {
+            for j in (i + 1)..n {
+                // i <= k <=j
+                let mut len_sub_str = 0;
+                // 区間[i,j]が回文か判定
+                for k in 0..(j - i + 1) {
+                    if str[i + k] == str[j - k] {
+                        len_sub_str += 1;
+                    } else {
+                        len_sub_str = -1;
+                        break;
+                    }
+                }
+                println!("{:?}",((i, j), len_sub_str));
+                len_list.push(((i, j), len_sub_str));
+            }
+        }
+
+        let mut max_len = -(1 << 30);
+        let (mut i_start, mut i_end) = (0, 0);
+        for x in len_list {
+            if max_len < x.1 {
+                max_len = x.1;
+                (i_start, i_end) = x.0;
+            }
+        }
+
+        ((i_start, i_end, max_len), s[i_start..(i_end + 1)].to_string())
+    }
+}
+
+// 模範解答
+struct SolutionAns {}
+impl SolutionAns {
+    pub fn longest_palindrome(s: String) -> String {
+        let s = s.chars().collect::<Vec<char>>();
+        let (mut left, mut right, length): (i32, i32, i32) = (0, 0, s.len() as i32);
+
+        if length == 1 {
+            return s[0].to_string();
+        }
+
+        for i in 0..length {
+            // odd length
+            let (mut l, mut r) = (i, i);
+
+            while l >= 0 && r < length && s[l as usize] == s[r as usize] {
+                if r - l > right - left {
+                    left = l;
+                    right = r;
+                }
+                l -= 1;
+                r += 1;
+            }
+
+            // even length
+            let (mut l, mut r) = (i, i + 1);
+
+            while l >= 0 && r < length && s[l as usize] == s[r as usize] {
+                if r - l > right - left {
+                    left = l;
+                    right = r;
+                }
+                l -= 1;
+                r += 1;
+            }
+        }
+
+        s[left as usize..=right as usize].iter().collect::<String>()
+    }
 }
 
 fn main() {
-    let case_1 = "babad".to_string(); 
-    // "bad" or "aba"
+    let case_1 = "babad".to_string();
+    // "bab" or "aba"
     let case_2 = "cbbd".to_string();
     // "bb"
 
-    println!("{}",Solution::palindrome_len(case_1.clone()));
-    println!("{}",Solution::palindrome_len(case_2.clone()));
+    println!("{}", Solution::palindrome_len(case_1.clone()));
+    println!("{}", Solution::palindrome_len(case_2.clone()));
+
+    println!("{:?}", Solution::palindrome_max_len(case_1.clone()));
+    println!("{:?}", Solution::palindrome_max_len(case_2.clone()));
+
+    /*
+    let hoge = "abcd";
+    println!("hoge:[0,2]: {}", hoge[0..2].to_string()); // ab
+    println!("hoge:[1,3]: {}", hoge[1..3].to_string()); // bc
+    */
 }
