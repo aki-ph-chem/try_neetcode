432786bbe0dfaad884936716d7f85656268d4166
aki <aki@mymail.com>
2023-12-25 19:53:05 +0900

    Coint Change II: 途中

---
+++ neet_code_150/src/two_d_dp/coin_change_2.rs
@@ -0,0 +1,15 @@
+struct Solution {}
+impl Solution {
+    pub fn change(amount: i32, coins: Vec<i32>) -> i32 {
+        3
+    }
+}
+
+fn main() {
+    let case_1 = (5, vec![1,2,5]);
+    // 4
+    let case_2 = (3, vec![2]);
+    // 0
+    let case_1 = (10, vec![10]);
+    // 1 
+}
