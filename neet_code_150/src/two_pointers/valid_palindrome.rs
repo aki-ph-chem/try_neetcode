8af1de463da96ce1e27a45766fbefffc3b0cf565
aki <aki@mymail.com>
2024-01-15 09:41:56 +0900

    Valid Palindrome:　別解を追加

--- neet_code_150/src/two_pointers/valid_palindrome.rs
+++ neet_code_150/src/two_pointers/valid_palindrome.rs
@@ -41,6 +41,32 @@ impl SolutionAns {
     }
 }
 
+// C++の模範解答より
+struct SolutionAnsCpp {}
+impl SolutionAnsCpp {
+    pub fn is_palindrome(s: String) -> bool {
+        let mut i = 0 as i32; 
+        let mut j = s.len() as i32 - 1;
+        let vec_char:Vec<char> = s.chars().collect();
+
+        while i < j {
+            while !vec_char[i as usize].is_alphanumeric() && i < j {
+                i += 1;
+            }
+            while !vec_char[j as usize].is_alphanumeric() && i < j {
+                j -= 1;
+            }
+            if vec_char[i as usize].to_lowercase().to_string() != vec_char[j as usize].to_lowercase().to_string() {
+                return false;
+            }
+            i += 1;
+            j -= 1;
+        }
+
+        true
+    }
+}
+
 fn main() {
     let case_1 = "A man, a plan, a canal: Panama".to_string();
     let case_2 = "race a car".to_string();
@@ -53,4 +79,8 @@ fn main() {
     println!("ase_1: {}", SolutionAns::is_palindrome(case_1.clone()));
     println!("ase_2: {}", SolutionAns::is_palindrome(case_2.clone()));
     println!("ase_3: {}", SolutionAns::is_palindrome(case_3.clone()));
+
+    println!("ase_1: {}", SolutionAnsCpp::is_palindrome(case_1.clone()));
+    println!("ase_2: {}", SolutionAnsCpp::is_palindrome(case_2.clone()));
+    println!("ase_3: {}", SolutionAnsCpp::is_palindrome(case_3.clone()));
 }
