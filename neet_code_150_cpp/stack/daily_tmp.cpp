91cb5f1df2fdd990fdde42a5be73cf350022076e
aki <aki@mymail.com>
2024-01-25 00:10:11 +0900

    Daily Temperatures: 模範解答を追加

--- neet_code_150_cpp/stack/daily_tmp.cpp
+++ neet_code_150_cpp/stack/daily_tmp.cpp
@@ -2,7 +2,8 @@
 #include <vector>
 #include <stack>
 
-class Solution {
+// Rustの模範解答より
+class SolutionAnsRust {
     public:
         std::vector<int> daily_tmp(const std::vector<int>& temperatures) {
             auto len_tmp = temperatures.size();
@@ -23,26 +24,73 @@ class Solution {
         }
 };
 
+// 模範解答
+class SolutionAns {
+    public:
+        std::vector<int> dailyTemperatures(const std::vector<int>& temperatures) {
+            int n = temperatures.size();
+
+            // pair: [index, temp]
+            std::stack<std::pair<int, int>> stk;
+            std::vector<int> result(n);
+
+            for (int i = 0; i < n; i++) {
+                int currDay = i;
+                int currTemp = temperatures[i];
+
+                while (!stk.empty() && stk.top().second < currTemp) {
+                    int prevDay = stk.top().first;
+                    int prevTemp = stk.top().second;
+                    stk.pop();
+
+                    result[prevDay] = currDay - prevDay;
+                }
+
+                stk.push({currDay, currTemp});
+            }
+
+            return result;
+        }
+};
+
 int main(void) {
     const auto case_1 = std::vector{73, 74, 75, 71, 69, 72, 76, 73};
     const auto case_2 = std::vector{30, 40, 50, 60};
     const auto case_3 = std::vector{30, 60, 90};
 
-    Solution s_1;
+    SolutionAnsRust s_ans_rust;
 
-    auto res_1 = s_1.daily_tmp(case_1);
-    for(const auto& v: res_1) {
+    auto res_1_rust = s_ans_rust.daily_tmp(case_1);
+    for(const auto& v: res_1_rust) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
 
-    auto res_2 = s_1.daily_tmp(case_2);
-    for(const auto& v: res_2) {
+    auto res_2_rust = s_ans_rust.daily_tmp(case_2);
+    for(const auto& v: res_2_rust) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
-    auto res_3 = s_1.daily_tmp(case_3);
-    for(const auto& v: res_3) {
+    auto res_3_rust = s_ans_rust.daily_tmp(case_3);
+    for(const auto& v: res_3_rust) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    SolutionAns s_ans;
+    auto res_1_ans = s_ans.dailyTemperatures(case_1);
+    for(const auto& v: res_1_ans) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+
+    auto res_2_ans = s_ans.dailyTemperatures(case_2);
+    for(const auto& v: res_2_ans) {
+        std::cout << v << " ";
+    }
+    std::cout << std::endl;
+    auto res_3_ans = s_ans.dailyTemperatures(case_3);
+    for(const auto& v: res_3_ans) {
         std::cout << v << " ";
     }
     std::cout << std::endl;
