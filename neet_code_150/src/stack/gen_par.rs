d9afacc6b65c12ae2928b0c9e2edca6949b7f4bb
aki <aki@mymail.com>
2024-01-24 15:18:28 +0900

    Generate Parentheses: 別解を追加

--- neet_code_150/src/stack/gen_par.rs
+++ neet_code_150/src/stack/gen_par.rs
@@ -4,10 +4,9 @@ struct Solution {}
 // n: 1 ~ 8
 impl Solution {
     pub fn gen_par(n: i32) -> Vec<String> {
-        let mut result:Vec<String> = vec![];
+        let mut result: Vec<String> = vec![];
 
-        for i in 0..n {
-        }
+        for i in 0..n {}
 
         result
     }
@@ -16,10 +15,11 @@ impl Solution {
 // 模範解答
 struct SolutionAns {}
 impl SolutionAns {
-pub fn generate_parenthesis(n: i32) -> Vec<String> {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
         // Using the function stack instead of an explicitly allocated Vec
         let mut res: Vec<String> = vec![];
 
+        // open, closeを両方ともnからスタートして0に等しくなるまで減らしながら再帰
         fn backtrack(res: &mut Vec<String>, s: String, open: i32, close: i32) {
             if open == 0 && close == 0 {
                 res.push(s);
@@ -42,10 +42,39 @@ pub fn generate_parenthesis(n: i32) -> Vec<String> {
     }
 }
 
+// C++の模範解答
+struct SolutionAnsCpp {}
+impl SolutionAnsCpp {
+    pub fn generate_parenthesis(n: i32) -> Vec<String> {
+        let mut res: Vec<String> = vec![];
+        Self::generate(n, 0, 0, "".to_string(), &mut res);
+
+        res
+    }
+
+    // open, closeを両方とも0からスタートしてnに等しくなるまで増やしながら再帰
+    fn generate(n: i32, open: i32, close: i32, str: String, result: &mut Vec<String>) {
+        if open == n && close == n {
+            result.push(str);
+            return;
+        }
+
+        if open < n {
+            Self::generate(n, open + 1, close, str.clone() + "(", result);
+        }
+        if open > close {
+            Self::generate(n, open, close + 1, str.clone() + ")", result);
+        }
+    }
+}
+
 fn main() {
     let case_1 = 3; // "[((()))","(()())","(())()","()(())","()()()"]
     let case_2 = 1; // "["()"]
                     //
     println!("case_1: {:?}", SolutionAns::generate_parenthesis(case_1));
     println!("case_2: {:?}", SolutionAns::generate_parenthesis(case_2));
+
+    println!("case_1: {:?}", SolutionAnsCpp::generate_parenthesis(case_1));
+    println!("case_2: {:?}", SolutionAnsCpp::generate_parenthesis(case_2));
 }
