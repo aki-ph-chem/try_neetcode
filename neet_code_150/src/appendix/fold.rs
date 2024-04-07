d325c4ed587f18a553b72d42aa756f3f93df56e4
aki <aki@mymail.com>
2024-04-07 14:11:21 +0900

    fold()の例を追加

--- neet_code_150/src/appendix/fold.rs
+++ neet_code_150/src/appendix/fold.rs
@@ -4,4 +4,18 @@ fn main() {
 
     let sum = (1..=10).clone().fold(1000, |acc, x| acc + x);
     println!("sum = {}", sum);
+
+    let array = vec![1, 3, 5, 0, 10, 6, 2, 3];
+    let max = array.iter().fold(i32::MIN, |res, x| res.max(*x));
+    let min = array.iter().fold(i32::MAX, |res, x| res.min(*x));
+
+    println!("max: {}", max);
+    println!("min: {}", min);
+
+    let array = vec![1.2, 3.01, 5.3, 0.02, 10.5, 6.2, 2.1, 3.5];
+    let max = array.iter().fold(f32::MIN, |res, x| res.max(*x));
+    let min = array.iter().fold(f32::MAX, |res, x| res.min(*x));
+
+    println!("max: {}", max);
+    println!("min: {}", min);
 }
