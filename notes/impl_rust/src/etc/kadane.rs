8e49d8486094fada3d3018fe075d2ea9868a4993
aki <aki@mymail.com>
2024-04-24 20:37:00 +0900

    Kadaneのalgorithmのメモに追記

--- notes/impl_rust/src/etc/kadane.rs
+++ notes/impl_rust/src/etc/kadane.rs
@@ -1,13 +1,24 @@
 // 和が最大となるような連続部分配列の和を返す
 pub fn max_subarray_sum(array: &Vec<i32>) -> i32 {
-    let (mut max, mut sum_positive) = (i32::MIN, 0);
+    let (mut max, mut sum_current) = (i32::MIN, 0);
     for v in array {
-        sum_positive += v;
-        max = max.max(sum_positive);
+        sum_current += v;
+        max = max.max(sum_current);
 
-        if sum_positive < 0 {
-            sum_positive = 0;
+        if sum_current < 0 {
+            sum_current = 0;
         }
+    }
+
+    max
+}
+
+// 上を少し工夫してスッキリさせた
+pub fn max_subarray_sum_2(array: &Vec<i32>) -> i32 {
+    let (mut max, mut sum_current) = (i32::MIN, 0);
+    for v in array {
+        sum_current = std::cmp::max(*v, sum_current + *v);
+        max = max.max(sum_current);
     }
 
     max
@@ -16,17 +27,17 @@ pub fn max_subarray_sum(array: &Vec<i32>) -> i32 {
 // 和が最大となるような連続配列の範囲をタプル((start,end), max_value)で返す
 pub fn max_sub_array_sum_range(array: &Vec<i32>) -> ((i32, i32), i32) {
     let (mut start, mut end) = (0, 0);
-    let (mut sum_idx, mut sum_positive, mut max) = (0, 0, i32::MIN);
+    let (mut sum_idx, mut sum_current, mut max) = (0, 0, i32::MIN);
 
     for (idx, v) in array.iter().enumerate() {
-        sum_positive += v;
+        sum_current += v;
 
-        if max < sum_positive {
-            (start, end, max) = (sum_idx, idx, sum_positive);
+        if max < sum_current {
+            (start, end, max) = (sum_idx, idx, sum_current);
         }
 
-        if sum_positive < 0 {
-            sum_positive = 0;
+        if sum_current < 0 {
+            sum_current = 0;
             sum_idx = idx + 1;
         }
     }
@@ -52,8 +63,16 @@ pub fn max_sub_array_sum_dp(array: &Vec<i32>) -> i32 {
 fn main() {
     let case_1 = vec![-2, -3, 4, -1, -2, 1, 5, -3];
     // => max: 7, range: (2,6)
+    let case_2 = vec![-2, -3, -4, -1, -2, -1, -5, -3];
+    // => max: -1,range(3,3)or range(6,6)
 
     println!("case_1: {}", max_subarray_sum(&case_1));
+    println!("case_1: {}", max_subarray_sum_2(&case_1));
     println!("case_1: {:?}", max_sub_array_sum_range(&case_1));
     println!("case_1: {}", max_sub_array_sum_dp(&case_1));
+
+    println!("case_2: {}", max_subarray_sum(&case_2));
+    println!("case_2: {}", max_subarray_sum_2(&case_2));
+    println!("case_2: {:?}", max_sub_array_sum_range(&case_2));
+    println!("case_2: {}", max_sub_array_sum_dp(&case_2));
 }
