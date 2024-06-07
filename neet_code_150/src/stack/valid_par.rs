b83091feca5a23fece97143d00f770a851b4971e
aki <aki@mymail.com>
2024-06-07 20:37:12 +0900

    Valid Parentheses: 後から解いたときの別解を追加

--- neet_code_150/src/stack/valid_par.rs
+++ neet_code_150/src/stack/valid_par.rs
@@ -161,6 +161,37 @@ impl SolutionAns {
     }
 }
 
+// 後で時間を置いて解いたときの解
+struct SolutionLatter;
+impl SolutionLatter {
+    pub fn is_valid(s: String) -> bool {
+        let mut stack = vec![];
+
+        for c in s.chars() {
+            if let Some(stack_top) = stack.last() {
+                match (stack_top, c) {
+                    ('{', '}') => {
+                        stack.pop();
+                    }
+                    ('(', ')') => {
+                        stack.pop();
+                    }
+                    ('[', ']') => {
+                        stack.pop();
+                    }
+                    _ => {
+                        stack.push(c);
+                    }
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+
+        stack.is_empty()
+    }
+}
+
 fn main() {
     let case_1 = "()".to_string();
     let case_2 = "()[]{}".to_string();
@@ -175,9 +206,9 @@ fn main() {
     println!("case_1: {}", Solution::is_valid_2(case_1.clone())); // true
     println!("case_2: {}", Solution::is_valid_2(case_2.clone())); // true
     println!("case_3: {}", Solution::is_valid_2(case_3.clone())); // false
-    // case_4の答えがおかしい
+                                                                  // case_4の答えがおかしい
     println!("case_4: {}", Solution::is_valid_2(case_4.clone())); // false
-                                                                  
+
     println!("case_1: {}", SolutionAns::is_valid(case_1.clone())); // true
     println!("case_2: {}", SolutionAns::is_valid(case_2.clone())); // true
     println!("case_3: {}", SolutionAns::is_valid(case_3.clone())); // false
@@ -191,4 +222,9 @@ fn main() {
     println!("(()()): {}", is_valid_maru("(()())"));
     println!("())()): {}", is_valid_maru("())())"));
     */
+
+    println!("case_1: {}", SolutionLatter::is_valid(case_1.clone())); // true
+    println!("case_2: {}", SolutionLatter::is_valid(case_2.clone())); // true
+    println!("case_3: {}", SolutionLatter::is_valid(case_3.clone())); // false
+    println!("case_4: {}", SolutionLatter::is_valid(case_4.clone())); // false
 }
