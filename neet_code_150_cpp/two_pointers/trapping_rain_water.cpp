2e9356cba7dcea086e0b51a2e2c4761c148202ae
aki <aki@mymail.com>
2024-07-24 00:44:53 +0900

    Trapping Rain Water: C++ 後で解いた解

--- neet_code_150_cpp/two_pointers/trapping_rain_water.cpp
+++ neet_code_150_cpp/two_pointers/trapping_rain_water.cpp
@@ -25,6 +25,30 @@ class SolutionAns {
         }
 };
 
+// 後で解いた解
+// AC
+class SolutionLatter {
+    public:
+        int trap(std::vector<int>& height) {
+            int result = 0;
+            int left = 0, right = height.size() - 1;
+            int left_max = 0, right_max = 0;
+            while(left < right) {
+                if(height[left] < height[right]) {
+                    left_max = std::max(left_max, height[left]);
+                    result += left_max - height[left];
+                    ++left;
+                } else if(height[left] >= height[right]) {
+                    right_max = std::max(right_max, height[right]);
+                    result += right_max - height[right];
+                    --right;
+                }
+            }
+
+            return result;
+        }
+};
+
 int main(void) {
     std::vector<int> case_1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
     // => 6
@@ -37,4 +61,9 @@ int main(void) {
     std::cout << s_ans.trap(case_1) << std::endl;
     std::cout << s_ans.trap(case_2) << std::endl;
     std::cout << s_ans.trap(case_3) << std::endl;
+
+    SolutionLatter s_ans_latter;
+    std::cout << s_ans_latter.trap(case_1) << std::endl;
+    std::cout << s_ans_latter.trap(case_2) << std::endl;
+    std::cout << s_ans_latter.trap(case_3) << std::endl;
 }
