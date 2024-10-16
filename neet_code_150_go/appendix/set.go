dcc07fd7c046e6d6b98e5c795f12302fd3841d2e
aki <aki@mymail.com>
2024-10-17 02:01:49 +0900

    文字をsetに入れたいとき:Go

--- neet_code_150_go/appendix/set.go
+++ neet_code_150_go/appendix/set.go
@@ -20,4 +20,14 @@ func main() {
 	for key := range set_string {
 		fmt.Printf("key: %s\n", key)
 	}
+
+	// charの代りにbyte
+	set_bytes := make(map[byte]bool)
+	set_bytes['h'] = true
+	set_bytes['o'] = true
+	set_bytes['g'] = true
+	set_bytes['e'] = true
+	for key := range set_bytes {
+		fmt.Printf("key: %c\n", key)
+	}
 }
