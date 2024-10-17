435c469ef6d12e2f39a3cf9959dd22e2cba7155e
aki <aki@mymail.com>
2024-10-17 14:02:33 +0900

    best_time_buy_sell.go: 別解を追加(Go)

--- neet_code_150_go/sliding_window/best_time_buy_sell.go
+++ neet_code_150_go/sliding_window/best_time_buy_sell.go
@@ -38,6 +38,23 @@ func maxProfit2(prices []int) int {
 	return result
 }
 
+// AC
+func maxProfit3(prices []int) int {
+	result := -1
+	min_list := make([]int, len(prices))
+	min_list[0] = prices[0]
+
+	for i := 1; i < len(prices); i++ {
+		min_list[i] = min(min_list[i-1], prices[i])
+	}
+
+	for i := 0; i < len(prices); i++ {
+		result = max(result, prices[i]-min_list[i])
+	}
+
+	return result
+}
+
 func main() {
 	case_1 := []int{7, 1, 5, 3, 6, 4}
 	case_2 := []int{7, 6, 4, 3, 1}
@@ -47,4 +64,7 @@ func main() {
 
 	fmt.Println("case_1: ", maxProfit2(case_1))
 	fmt.Println("case_2: ", maxProfit2(case_2))
+
+	fmt.Println("case_1: ", maxProfit3(case_1))
+	fmt.Println("case_2: ", maxProfit3(case_2))
 }
